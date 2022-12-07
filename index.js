57dc93e5ec28bffd2aa3b149780473f7cbfa4954
Andre Dragacevac <a.dragacevac@icloud.com>
2022-12-07 00:03:03 -0500

    index.js file

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ function renderBrand(brand){
     const img = document.createElement('img');
     img.src = brand.image;
     img.alt = `${brand.name} logo`;
+    img.className = brand.name;
 
     li.append(img);
     document.querySelector('#brand-list').append(li);
+
+    img.addEventListener('click', (e)=> console.log("jordans clicked"));
+}
+//click on brand image and filter array
+function filterShoesByBrand(shoe){
+    var jordansArray = shoes.filter(function (shoe) {
+        return shoe.brand = "Air Jordan"
+    });
 }
 getJSON("http://localhost:3000/brands")
   .then((brands) => {
     brands.forEach(brand => renderBrand(brand))
 })
-// fetch('http://localhost:3000/brands')
-// .then((response) => response.json())
-// .then((brands) => brands.forEach(renderBrand));
+
 function getJSON(url) {
     return fetch(url)
       .then(response => {
